5bbbe9a163bb25e0633bc9aa9f96c8956651c49d
Gabrieeeel123 <104031898+Gabrieeeel123@users.noreply.github.com>
2022-05-20 13:15:19 -0500

    Add files via upload

---
+++ Interfaces/Class3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class main
+    {
+        public static void Main(string[] args)
+        {
+            Vehiculos carro1 = new Vehiculos(); 
+            Console.WriteLine(carro1.getInfoVehiculo());
+          
+
+
+        }
+    }
+}
